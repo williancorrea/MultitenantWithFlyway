04fa844f0fafb1e6d80e98d4bb894d5513a364d0
Willian Vagner Vicente Corrêa <willian.correa@cits.br>
2024-10-14 10:46:37 -0300

    Adding initial structure

---
+++ src/test/java/br/com/devcansado/multitenant/MultitenantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devcansado.multitenant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
